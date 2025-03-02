2de20859e75c586a53459ee4d00b16a3c8917a3a
hannibal <cds730@naver.com>
2025-03-02 21:59:59 +0900

    rrt,legoloam 2nd (cds)

---
+++ src/ma_rrt_path_plan/src/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import rospy
+from MaRRTPathPlanNode import MaRRTPathPlanNode
+
+def main():
+    rospy.init_node('MaRRTPathPlanNode')
+    maRRTPathPlanNode = MaRRTPathPlanNode()
+
+    rate = rospy.Rate(20)
+
+    while not rospy.is_shutdown():
+        maRRTPathPlanNode.sampleTree()
+        rate.sleep()
+
+if __name__ == '__main__':
+    main()
