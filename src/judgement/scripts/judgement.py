d580beafe108db8650a873384c4b99a5faa4fcac
Highsky7 <albert31115@gmail.com>
2025-03-08 21:04:54 +0900

    revising this file to publish throttle to arduino(lsm)

--- src/judgement/scripts/judgement.py
+++ src/judgement/scripts/judgement.py
@@ -2,25 +2,28 @@
 import rospy
 from std_msgs.msg import Float32, Bool
 
-class SteeringJudgement:
+class Judgement:
     def __init__(self):
         # 구독자 설정: ROS 토픽에서 데이터를 수신
         self.sub_lane = rospy.Subscriber('auto_steer_angle_lane', Float32, self.lane_callback)
         self.sub_cone = rospy.Subscriber('auto_steer_angle_cone', Float32, self.cone_callback)
         self.sub_tunnel = rospy.Subscriber('auto_steer_angle_tunnel', Float32, self.tunnel_callback)
         self.sub_lane_status = rospy.Subscriber('lane_detection_status', Bool, self.lane_status_callback)
-        
+
         # 퍼블리셔 설정: 판단된 조향각을 발행
         self.pub_steering = rospy.Publisher("steering_angle", Float32, queue_size=10)
-        
+
+        # 퍼블리셔 설정: 쓰로틀값을 발행
+        self.pub_throttle = rospy.Publisher("auto_throttle", Float32, queue_size=10)
+
         # 변수 초기화
         self.lane_angle = None  # 차선 기반 조향각
         self.cone_angle = None  # 콘 기반 조향각
         self.tunnel_angle = None  # 터널 기반 조향각
         self.lane_detected = False  # 차선 감지 상태
-        
+
         # 주기적 퍼블리시를 위한 타이머 (10Hz)
-        self.timer = rospy.Timer(rospy.Duration(0.1), self.publish_steering)
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.timer_callback)
 
     def lane_callback(self, msg):
         """차선 조향각 콜백 함수"""
@@ -37,6 +40,11 @@ class SteeringJudgement:
     def lane_status_callback(self, msg):
         """차선 감지 상태 콜백 함수"""
         self.lane_detected = msg.data
+
+    def publish_throttle(self, event):
+        """쓰로틀 퍼블리시 로직"""
+        throttle = 0.4
+        self.pub_throttle.publish(Float32(data=throttle))
 
     def publish_steering(self, event):
         """조향각 판단 및 퍼블리시 로직"""
@@ -56,12 +64,15 @@ class SteeringJudgement:
             rospy.loginfo("Using tunnel angle: %.2f", steering_angle)
         else:
             rospy.logwarn("No valid steering angle available")
-
-        # 유효한 조향각이 있으면 퍼블리시
+         # 유효한 조향각이 있으면 퍼블리시
         if steering_angle is not None:
             self.pub_steering.publish(Float32(data=steering_angle))
 
+    def timer_callback(self, event):
+        self.publish_steering(event)
+        self.publish_throttle(event)
+
 if __name__ == '__main__':
-    rospy.init_node("steering_judgement_node")
-    SteeringJudgement()
+    rospy.init_node("judgement_node")
+    Judgement()
     rospy.spin()
