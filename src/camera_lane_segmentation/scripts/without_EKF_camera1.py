a946ebc92d1d66b934db5436ac034275dd9d838a
yoosm321 <smzzang21@konkuk.ac.kr>
2025-03-12 14:15:00 +0900

    change matplot scale(ysm)

--- src/camera_lane_segmentation/scripts/without_EKF_camera1.py
+++ src/camera_lane_segmentation/scripts/without_EKF_camera1.py
@@ -47,7 +47,7 @@ def make_parser():
     parser.add_argument('--weights', type=str, default='./yolopv2.pt', help='model.pt 경로')
     parser.add_argument('--source', type=str,
                         #default='2',
-                        default='/home/yoo/source/test_video3.mp4',
+                        default='/home/yoo/source/test_video1.mp4',
                         help='source: 0(webcam) 또는 파일 경로')
     parser.add_argument('--img-size', type=int, default=640, help='YOLO 추론 해상도')
     parser.add_argument('--device', default='0', help='cuda device: 0 또는 cpu')
@@ -313,8 +313,8 @@ def debug_plot_lane(path_points, lane_data=None, goal_point=None):
     plt.title("Lane & Path Functions in Vehicle Coordinates")
     plt.legend()
     plt.gca().invert_xaxis()
-    plt.xlim(3.0, -3.0)
-    plt.ylim(0.0, 6.0)
+    plt.xlim(1.0, -1.0)
+    plt.ylim(0.0, 3.0)
     plt.grid(True)
     plt.show(block=False)
     plt.pause(0.001)
