69a4ed1d3116facb9278b082b63b9a8771ce50b7
Highsky7 <albert31115@gmail.com>
2025-03-17 00:12:37 +0900

    Avoid merge(LSM)

--- src/camera_lane_segmentation/scripts/without_EKF_camera1.py
+++ src/camera_lane_segmentation/scripts/without_EKF_camera1.py
@@ -46,7 +46,7 @@ def make_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default='./yolopv2.pt', help='model.pt 경로')
     parser.add_argument('--source', type=str,
-                        default='2',
+                        default='0',
                         # default='/home/yoo/source/test_video4.mp4',
                         help='source: 0(webcam) 또는 파일 경로')
     parser.add_argument('--img-size', type=int, default=640, help='YOLO 추론 해상도')
