67f6b1967b8ff7283059dc2b2c0e3d7b1c356583
hannibal <cds730@naver.com>
2025-04-17 00:04:34 +0900

    Resolve merge/revert conflicts

--- src/voxelnext_pkg/voxelnet_test_pcd.py
+++ src/voxelnext_pkg/voxelnet_test_pcd.py
@@ -1,16 +1,4 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-#!/home/hannibal/anaconda3/envs/voxelnext/bin/python3
-=======
 #!/usr/bin/env python3
->>>>>>> Stashed changes
-=======
-#!/usr/bin/env python3
->>>>>>> Stashed changes
-=======
-#!/usr/bin/env python3
->>>>>>> Stashed changes
 
 import torch
 import numpy as np
