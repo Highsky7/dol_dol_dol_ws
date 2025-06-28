bae79dc7b778e63e4288f3e8be1dfd0a0b5d984d
hannibal <cds730@naver.com>
2025-06-28 20:35:04 +0900

    feat: Add dynamic_static_classifier (cds)

--- src/dynamic_static_pkg/src/dynamic_static_classifier.cpp
+++ src/dynamic_static_pkg/src/dynamic_static_classifier.cpp
@@ -1,109 +1,104 @@
-// src/dynamic_static_classifier.cpp
-
 #include <ros/ros.h>
-#include <tf2_ros/transform_listener.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-
 #include <message_filters/subscriber.h>
-#include <message_filters/synchronizer.h>
-#include <message_filters/sync_policies/approximate_time.h>
-
+#include <message_filters/cache.h>  
 #include <dynamic_static_pkg/TrackedObjects.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <cmath>
 
-// alias the policy & synchronizer
-using SyncPolicy   = message_filters::sync_policies::ApproximateTime<
-                        dynamic_static_pkg::TrackedObjects,
-                        geometry_msgs::TwistStamped
-                    >;
-using Synchronizer = message_filters::Synchronizer<SyncPolicy>;
+using namespace message_filters;
 
-class DynamicStaticClassifier {
+class DynamicStaticClassifier
+{
 public:
   DynamicStaticClassifier()
-    : tf_listener_(tf_buffer_)
+    : v_thresh_(0.3)
   {
-    // subscribe the two streams
-    tracks_sub_.subscribe(nh_, "/tracked_objects", 10);
-    vel_sub_   .subscribe(nh_, "/utm/vel",           10);
+    ros::NodeHandle pnh("~");
+    pnh.param("v_thresh", v_thresh_, v_thresh_);
+    ROS_INFO("v_thresh set to %.3f m/s", v_thresh_);
 
-    // use the "queue size" constructor to select the right overload:
-    sync_.reset(new Synchronizer(50, tracks_sub_, vel_sub_));
-    sync_->registerCallback(
-      boost::bind(&DynamicStaticClassifier::callback, this, _1, _2));
+    // ① /tracked_objects 구독
+    tracked_sub_.subscribe(nh_, "/tracked_objects", 10);
 
-    pub_static_  = nh_.advertise<visualization_msgs::MarkerArray>("/static_objects", 1);
-    pub_dynamic_ = nh_.advertise<visualization_msgs::MarkerArray>("/dynamic_objects",1);
+    // ② /utm/vel 구독 + Cache 설정 (큐 크기 50, 데이터 보관 기간 제한 없음)
+    vel_sub_.subscribe(nh_, "/utm/vel", 50);
+    vel_cache_.reset(new Cache<geometry_msgs::TwistStamped>(vel_sub_, 50));
 
-    nh_.param("v_thresh", v_thresh_, 1.2);
+    // ③ tracked_objects 콜백만 등록
+    tracked_sub_.registerCallback(
+      boost::bind(&DynamicStaticClassifier::trackedCallback, this, _1)
+    );
+
+    // 퍼블리셔 설정
+    pub_static_  = nh_.advertise<visualization_msgs::MarkerArray>("/static_objects",  1);
+    pub_dynamic_ = nh_.advertise<visualization_msgs::MarkerArray>("/dynamic_objects", 1);
   }
 
 private:
-  void callback(const dynamic_static_pkg::TrackedObjectsConstPtr& tracks,
-                const geometry_msgs::TwistStampedConstPtr& gps_vel)
+  void trackedCallback(const dynamic_static_pkg::TrackedObjectsConstPtr& tracks)
   {
+    // 1) 해당 시점 근처 속도 메시지들 검색 (±5초 범위)
+    ros::Time t = tracks->header.stamp;
+    auto vel_msgs = vel_cache_->getInterval(t - ros::Duration(5.0),
+                                            t + ros::Duration(5.0));
 
-    ROS_INFO("dynamic_static_classifier CALLBACK entered: #tracks=%zu, utm_vel=(%.2f,%.2f)",
-            tracks->id.size(),
-            gps_vel->twist.linear.x,
-            gps_vel->twist.linear.y);
-    // ego–motion compensation
-    geometry_msgs::Vector3Stamped ego_utm, ego_lidar;
-    ego_utm.header = gps_vel->header;
-    ego_utm.vector = gps_vel->twist.linear;
-    try {
-      tf_buffer_.transform(ego_utm, ego_lidar, "velodyne", ros::Duration(0.05));
-    } catch (tf2::TransformException &ex) {
-      ROS_WARN("TF failed: %s", ex.what());
-      return;
+    // 2) 가장 최근 메시지 선택 (없으면 zero)
+    geometry_msgs::TwistStamped vel;
+    if (!vel_msgs.empty()) {
+      vel = *vel_msgs.back();
     }
 
-    visualization_msgs::MarkerArray statics, dynamics;
-    for (size_t i = 0; i < tracks->id.size(); ++i) {
-      int id       = tracks->id[i];
-      auto center  = tracks->center[i];
-      float vx_obj = tracks->vx[i];
-      float vy_obj = tracks->vy[i];
+    visualization_msgs::MarkerArray static_markers, dynamic_markers;
 
-      float rvx = vx_obj - ego_lidar.vector.x;
-      float rvy = vy_obj - ego_lidar.vector.y;
-      float res_speed = std::hypot(rvx, rvy);
+    for (size_t i = 0; i < tracks->id.size(); ++i)
+    {
+      // (예시) 객체 속력 계산
+      float obj_speed = std::hypot(tracks->vx[i], tracks->vy[i]);
+      // 속도 메시지의 평면 속도 (필요 시 활용)
+      float utm_vx = vel.twist.linear.x;
+      float utm_vy = vel.twist.linear.y;
 
+      // Marker 생성
       visualization_msgs::Marker m;
-      m.header        = tracks->header;
-      m.ns            = "dyn_stat";
-      m.id            = id;
-      m.type          = visualization_msgs::Marker::CUBE;
-      m.action        = visualization_msgs::Marker::ADD;
-      m.pose.position = center;
-      m.scale.x = 0.5; m.scale.y = 0.5; m.scale.z = 1.0;
-      m.color.a = 0.6;
-      if (res_speed < v_thresh_) {
-        m.color.r = 0; m.color.g = 1; m.color.b = 0;
-        statics.markers.push_back(m);
+      m.header = tracks->header;
+      m.ns     = (obj_speed > v_thresh_) ? "dynamic" : "static";
+      m.id     = tracks->id[i];
+      m.type   = visualization_msgs::Marker::CUBE;
+      m.pose.position = tracks->center[i];
+      m.scale.x = m.scale.y = m.scale.z = 1.0;
+      m.color.a = 0.8;
+      if (obj_speed > v_thresh_) {
+        m.color.r = 1.0;  // 동적: 빨강
       } else {
-        m.color.r = 1; m.color.g = 0; m.color.b = 0;
-        dynamics.markers.push_back(m);
+        m.color.g = 1.0;  // 정적: 초록
       }
+
+      if (obj_speed > v_thresh_)
+        dynamic_markers.markers.push_back(m);
+      else
+        static_markers.markers.push_back(m);
     }
-    pub_static_.publish(statics);
-    pub_dynamic_.publish(dynamics);
+
+    // 퍼블리시
+    pub_static_.publish(static_markers);
+    pub_dynamic_.publish(dynamic_markers);
   }
 
   ros::NodeHandle nh_;
-  tf2_ros::Buffer           tf_buffer_;
-  tf2_ros::TransformListener tf_listener_;
-
-  message_filters::Subscriber<dynamic_static_pkg::TrackedObjects> tracks_sub_;
-  message_filters::Subscriber<geometry_msgs::TwistStamped>        vel_sub_;
-  boost::shared_ptr<Synchronizer> sync_;
-
-  ros::Publisher pub_static_, pub_dynamic_;
   double v_thresh_;
+
+  // message_filters subscribers & cache
+  Subscriber<dynamic_static_pkg::TrackedObjects> tracked_sub_;
+  Subscriber<geometry_msgs::TwistStamped>        vel_sub_;
+  boost::shared_ptr<Cache<geometry_msgs::TwistStamped>> vel_cache_;
+
+  // publishers
+  ros::Publisher pub_static_, pub_dynamic_;
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
   ros::init(argc, argv, "dynamic_static_classifier");
   DynamicStaticClassifier node;
   ros::spin();
