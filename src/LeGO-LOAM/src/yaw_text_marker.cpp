200b44ebd6175814e42c573f87b1b00b2c1c4570
hannibal <cds730@naver.com>
2025-07-01 02:22:27 +0900

    refactor: Remove benzz from yaw file (cds)

--- src/LeGO-LOAM/src/yaw_text_marker.cpp
+++ src/LeGO-LOAM/src/yaw_text_marker.cpp
@@ -14,21 +14,13 @@ public:
     pnh.param("color_g", color_g_, 1.0);
     pnh.param("color_b", color_b_, 0.0);
 
-    // 구독자 추가
-    sub_legoloam_ = nh.subscribe("/legoloam_yaw_corrected_deg", 1,
-                                 &YawTextMarker::yawLegoloamCb, this);
-
-    sub_yaw_ = nh.subscribe("/global_yaw", 1,
-                            &YawTextMarker::yawCb, this);
-
-    // 퍼블리셔 추가
-    pub_legoloam_ = nh.advertise<visualization_msgs::Marker>("legoloam_yaw_corrected_deg_marker", 1);
-    pub_yaw_ = nh.advertise<visualization_msgs::Marker>("global_yaw_deg_marker", 1);
+    sub_ = nh.subscribe("/legoloam_yaw_corrected_deg", 1,
+                        &YawTextMarker::yawCb, this);
+    pub_ = nh.advertise<visualization_msgs::Marker>("legoloam_yaw_corrected_deg_marker", 1);
   }
 
 private:
-  void yawLegoloamCb(const std_msgs::Float64& msg) {
-    // legoloam_yaw 텍스트 마커
+  void yawCb(const std_msgs::Float64& msg) {
     visualization_msgs::Marker m;
     m.header.frame_id = "velodyne";        // velodyne 프레임 원점
     m.header.stamp    = ros::Time::now();
@@ -42,52 +34,21 @@ private:
     m.pose.position.y = 0.0;   // 예: y = 2.0 m
     m.pose.position.z = 3.5;   // 예: z = 0.5 m (지면 위 0.5 m)
     m.pose.orientation.w = 1.0;            // 방향 무시
-    m.scale.z = 4.0;                       // 글씨 크기(m 단위)
+    m.scale.z = 3.0;                       // 글씨 크기(m 단위)
     m.color.a = 1.0;
     m.color.r = color_r_;
     m.color.g = color_g_;
     m.color.b = color_b_;
 
     std::ostringstream ss;
-    ss << std::fixed << std::setprecision(precision_) << "legoloam_yaw_corrected_deg " << msg.data << "°";
+    ss << std::fixed << std::setprecision(precision_)<< "legoloam_yaw" << " " << msg.data << "°";
     m.text = ss.str();
 
-    pub_legoloam_.publish(m);
+    pub_.publish(m);
   }
 
-  void yawCb(const std_msgs::Float64& msg) {
-    // global_yaw 텍스트 마커 (빨간색)
-    visualization_msgs::Marker m;
-    m.header.frame_id = "velodyne";        // velodyne 프레임 원점
-    m.header.stamp    = ros::Time::now();
-    m.ns   = "yaw_text";
-    m.id   = 1;  // 다른 id로 구분
-    m.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
-    m.action = visualization_msgs::Marker::ADD;
-
-    // 텍스트
-    m.pose.position.x = 1.5;   // 예: x = 1.0 m
-    m.pose.position.y = 0.0;   // 예: y = 2.0 m
-    m.pose.position.z = 0.5;   // 예: z = 0.5 m (지면 위 0.5 m)
-    m.pose.orientation.w = 1.0;            // 방향 무시
-    m.scale.z = 2.0;                       // 글씨 크기(m 단위)
-    m.color.a = 1.0;
-    m.color.r = 1.0;  // 빨간색
-    m.color.g = 0.0;
-    m.color.b = 0.0;
-
-    std::ostringstream ss;
-    ss << std::fixed << std::setprecision(precision_) << "global_yaw_deg " << msg.data << "°";
-    m.text = ss.str();
-
-    pub_yaw_.publish(m);
-  }
-
-  ros::Subscriber sub_legoloam_;
-  ros::Subscriber sub_yaw_;
-  ros::Publisher pub_legoloam_;
-  ros::Publisher pub_yaw_;
-
+  ros::Subscriber sub_;
+  ros::Publisher  pub_;
   int precision_;
   double color_r_, color_g_, color_b_;
 };
