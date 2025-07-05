4aaf2cf9d391c23dcf03002ccb5f8e5d6ab85fb5
hannibal <cds730@naver.com>
2025-07-05 10:19:39 +0900

    feat: weighted speed calcul

--- src/dynamic_static_pkg/src/dynamic_static_classifier.cpp
+++ src/dynamic_static_pkg/src/dynamic_static_classifier.cpp
@@ -20,7 +20,11 @@ public:
       alpha_(0.05), // EMA 필터 강도 강화
       prev_v_vehicle_x_(0.0),
       prev_v_vehicle_y_(0.0),
-      prev_global_yaw_(0.0)
+      prev_global_yaw_(0.0),
+      prev_auto_throttle_(0.0),      // 추가: 이전 오토쓰로틀 값
+      have_auto_throttle_(false),     // 추가: 오토쓰로틀 수신 여부
+      weight_x_(0.5), // 추가: x축 속도 가중치
+      weight_y_(30.0)  // 추가: y축 속도 가중치
   {
     ros::NodeHandle pnh("~");
     pnh.param("v_thresh", v_thresh_, v_thresh_);
@@ -30,6 +34,7 @@ public:
     tracked_sub_.subscribe(nh_, "/tracked_objects", 10);
     raw_vel_sub_ = nh_.subscribe("/utm/vel", 10, &DynamicStaticClassifier::velRawCallback, this);
     yaw_sub_ = nh_.subscribe("/global_yaw", 10, &DynamicStaticClassifier::yawCallback, this);
+    throttle_sub_ = nh_.subscribe("/auto_throttle", 10, &DynamicStaticClassifier::throttleCallback, this); // 추가: 구독 설정
     tracked_sub_.registerCallback(boost::bind(&DynamicStaticClassifier::trackedCallback, this, _1));
 
     pub_static_ = nh_.advertise<visualization_msgs::MarkerArray>("/static_objects", 1);
@@ -54,6 +59,14 @@ private:
     prev_v_vehicle_y_ = alpha_ * last_vel_.twist.linear.y + (1.0 - alpha_) * prev_v_vehicle_y_;
     have_vel_ = true;
     ROS_DEBUG("Filtered vehicle vel: vx=%.3f, vy=%.3f", prev_v_vehicle_x_, prev_v_vehicle_y_);
+  }
+
+  void throttleCallback(const std_msgs::Float32::ConstPtr& msg)
+  {
+    auto_throttle_ = alpha_ * msg->data + (1.0 - alpha_) * prev_auto_throttle_;
+    prev_auto_throttle_ = auto_throttle_;
+    have_auto_throttle_ = true;
+    ROS_DEBUG("Filtered auto_throttle: %.3f m/s", auto_throttle_);
   }
 
   void trackedCallback(const dynamic_static_pkg::TrackedObjectsConstPtr& tracks)
@@ -96,16 +109,21 @@ private:
       if (have_vel_) {
         double cos_yaw = std::cos(-global_yaw_);
         double sin_yaw = std::sin(-global_yaw_);
-        float v_vehicle_velo_x = cos_yaw * prev_v_vehicle_x_ + sin_yaw * prev_v_vehicle_y_;
-        float v_vehicle_velo_y = -sin_yaw * prev_v_vehicle_x_ + cos_yaw * prev_v_vehicle_y_;
-        abs_vx = obj_vx - v_vehicle_velo_x; // 차량 속도 빼기
-        abs_vy = obj_vy - v_vehicle_velo_y;
-        ROS_DEBUG("Vehicle vel (Velodyne): vx=%.3f, vy=%.3f", v_vehicle_velo_x, v_vehicle_velo_y);
+        // float v_vehicle_velo_x = cos_yaw * prev_v_vehicle_x_ + sin_yaw * prev_v_vehicle_y_;
+        // float v_vehicle_velo_y = -sin_yaw * prev_v_vehicle_x_ + cos_yaw * prev_v_vehicle_y_;
+        // abs_vx = obj_vx - v_vehicle_velo_x; // 차량 속도 빼기
+        // 만약 /auto_throttle 토픽에서 데이터를 받은 적이 없으면(have_auto_throttle_ == false), auto_throttle_을 빼지 않고 obj_vx를 그대로 사용합니다. (0.0을 빼므로 결과는 동일).
+        // abs_vy = obj_vy - v_vehicle_velo_y;
+        abs_vx = obj_vx - (have_auto_throttle_ ? auto_throttle_ : 0.0); // 수정: auto_throttle_ 빼기
+        abs_vy = obj_vy;
+        // ROS_DEBUG("Vehicle vel (Velodyne): vx=%.3f, vy=%.3f", v_vehicle_velo_x, v_vehicle_velo_y);
       } else {
         abs_vx = obj_vx;
         abs_vy = obj_vy;
       }
-      float obj_speed = std::hypot(abs_vx, abs_vy);
+      // float obj_speed = std::hypot(abs_vx, abs_vy);
+      // std::hypot(x, y)는 두 값의 유클리드 거리(즉, $\sqrt{x^2 + y^2}$)를 계산하는 C++ 표준 라이브러리 함수
+      float obj_speed = std::sqrt(weight_x_ * abs_vx * abs_vx + weight_y_ * abs_vy * abs_vy); // 수정: 가중치 적용
 
       bool is_static_by_position = false;
       if (last_centers_.find(tracks->id[i]) != last_centers_.end()) {
@@ -146,9 +164,9 @@ private:
       text_marker.id = tracks->id[i];
       text_marker.action = visualization_msgs::Marker::ADD;
       text_marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
-      text_marker.pose.position.x = tracks->center[i].x - 0.35 ;
-      text_marker.pose.position.y = tracks->center[i].y + 1.2 ;
-      text_marker.pose.position.z = tracks->center[i].z + 0.5 ;
+      text_marker.pose.position.x = tracks->center[i].x - 0.35;
+      text_marker.pose.position.y = tracks->center[i].y + 1.2;
+      text_marker.pose.position.z = tracks->center[i].z + 0.5;
       text_marker.pose.orientation.w = 1.0;
       std::stringstream ss;
       ss << "abs_vx: " << std::fixed << std::setprecision(3) << abs_vx
@@ -186,6 +204,12 @@ private:
   ros::Publisher pub_static_, pub_dynamic_, pub_absolute_vx_vy_;
   std::set<int> last_ids_;
   std::map<int, geometry_msgs::Point> last_centers_;
+  ros::Subscriber throttle_sub_;     // 추가
+  float auto_throttle_;              // 추가
+  float prev_auto_throttle_;         // 추가
+  bool have_auto_throttle_;          // 추가
+  float weight_x_;                   // 추가: x축 속도 가중치
+  float weight_y_;                   // 추가: y축 속도 가중치
 };
 
 int main(int argc, char** argv)
